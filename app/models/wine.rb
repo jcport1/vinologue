3c15931f1803575692c2dad19013aaa9c45fb3ae
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-12-12 21:16:19 -0500

    added validation to wine model

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -3,6 +3,6 @@ class Wine < ActiveRecord::Base
    has_many :reviews
    has_many :users, through: :reviews 
 
-   # validates :name, :year, :varietal, :wine_style, presence: true 
+   validates :name, :year, :varietal, :wine_style, presence: true 
 
 end 
