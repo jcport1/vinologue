ccbf066b69eeec17f5ac48e087ec32ee03060e8f
jeffersoncuartas <jefferson.cuartas@gmail.com>
2020-12-21 12:33:23 -0500

    updates

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -3,6 +3,6 @@ class Wine < ActiveRecord::Base
    belongs_to :user
    belongs_to :wine_type
 
-   validates :name, :year, :varietal, :wine_type_id, presence: true 
+   validates :name, :year, :varietal, :quantity, :wine_type_id, presence: true 
 
 end 
